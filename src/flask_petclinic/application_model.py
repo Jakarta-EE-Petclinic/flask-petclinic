71fff42dd7bd7a5530b583a3eb17cd211fc2d615
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-28 22:43:18 +0100

    setup ...

--- src/flask_petclinic/application_model.py
+++ src/flask_petclinic/application_model.py
@@ -19,9 +19,9 @@ class ApplicationPage:
 
 
 class Owner(db.Model):
-    __tablename__ = 'application_region'
+    __tablename__ = 'petclinic_owner'
     __table_args__ = (
-        db.UniqueConstraint('region', name='uix_application_region'),
+        db.UniqueConstraint('region', 'lastName', 'zipCode', name='uix_petclinic_owner'),
     )
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(255), nullable=False, unique=True)
@@ -33,10 +33,7 @@ class Owner(db.Model):
     zipCode = db.Column(db.String(255), nullable=False, unique=True)
     phoneNumber = db.Column(db.String(255), nullable=False, unique=True)
     phoneNumber = db.Column(db.String(255), nullable=False, unique=True)
-
-    pets
-
-
+    pets = db.relationship("Pet", primaryjoin="Owner.id==Visit.owner_id")
 
     @classmethod
     def create_new_object_factory(cls, my_date_rep):
@@ -61,44 +58,53 @@ class Owner(db.Model):
 
 
 class Pet(db.Model):
-    __tablename__ = 'application_region'
+    __tablename__ = 'petclinic_pet'
     __table_args__ = (
-        db.UniqueConstraint('region', name='uix_application_region'),
+        db.UniqueConstraint('name', name='uix_petclinic_pet'),
     )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
-    petType
-    owner
-    visits
+    petType_id = db.Column(db.Integer, db.ForeignKey('petclinic_pet_type.id'), nullable=False)
+    petType = db.relationship(
+        'PetType',
+        foreign_keys=[petType_id],
+        lazy='joined',
+        cascade='all, delete',
+        order_by='PetType.name')
+    owner_id = db.Column(db.Integer, db.ForeignKey('petclinic_owner.id'), nullable=False)
+    owner = db.relationship('Owner', foreign_keys="[Pet.owner_id]",
+                            lazy='joined', cascade='all, delete', order_by='Owner.lastName')
+    visits = db.relationship("Visit", primaryjoin="Pet.id==Visit.pet_id")
 
 
 class PetType(db.Model):
-    __tablename__ = 'application_region'
+    __tablename__ = 'petclinic_pet_type'
     __table_args__ = (
-        db.UniqueConstraint('region', name='uix_application_region'),
+        db.UniqueConstraint('name', name='uix_petclinic_pet_type'),
     )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
 
 
 class Specialty(db.Model):
-    __tablename__ = 'application_region'
+    __tablename__ = 'petclinic_specialty'
     __table_args__ = (
-        db.UniqueConstraint('region', name='uix_application_region'),
+        db.UniqueConstraint('name', name='uix_petclinic_specialty'),
     )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
-
+    # n-to-m
 
 class Vet(db.Model):
     __tablename__ = 'application_region'
     __table_args__ = (
-        db.UniqueConstraint('region', name='uix_application_region'),
+        db.UniqueConstraint('firstName', 'lastName', name='uix_application_region'),
     )
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(255), nullable=False, unique=True)
     lastName = db.Column(db.String(255), nullable=False, unique=True)
-    specialties
+    # n-to-m
+    # specialties = db.relationship("Specialty", primaryjoin="Vet.id==Specialty.owner_id")
 
 
 class Visit(db.Model):
@@ -109,4 +115,7 @@ class Visit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(255), nullable=False, unique=True)
     description = db.Column(db.String(255), nullable=False, unique=True)
-    pet
+    pet_id = db.Column(db.Integer, db.ForeignKey('petclinic_pet.id'), nullable=False)
+    pet = db.relationship('Pet', foreign_keys="[Visit.pet_id]",
+                            lazy='joined', cascade='all, delete', order_by='Pet.name')
+
